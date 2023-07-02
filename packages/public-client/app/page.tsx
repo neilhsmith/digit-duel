35078546dfdaf3f0c680130c69c9c65a6e6f3413
Neil Smith <me+github@neilsmith.fyi>
2023-07-01 21:57:17 -0500

    add public-client project

---
+++ packages/public-client/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <p>
+        this is a statically generated nextjs app running on an Azure Static Web
+        App (or locally via npm run dev). we&apos;re using nextjs because
+        there&apos;s a good chance we&apos;ll convert this to a hybrid app in
+        the future and can simply add the new features and swap its deployment
+        to something that handles server-side rendering.
+      </p>
+    </main>
+  )
+}
---
+++ packages/public-client/app/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  extend: {},
+  colors: {
+    currentColor: "currentColor",
+    transparent: "transparent",
+
+    white: "#fff",
+    black: {
+      transparent: "rgba(0, 0, 0, .25)",
+      900: "#282828",
+      950: "#000",
+    },
+  },
+  fontFamily: {
+    sans: ["var(--font-inter)"],
+    mono: ["var(--font-roboto-mono)"],
+  },
+}
+
+export default theme
